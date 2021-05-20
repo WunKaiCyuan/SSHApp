7cc13bb8769e3e2c32fcd709a943cfa706c3c3d8
cyuan <pijhs70809@gmail.com>
2021-05-21 03:55:19 +0800

    fix username、address error and separate connect、execute functions and change classpath

--- app/src/main/java/com/lilith/android/SSHApp/MainActivity.java
+++ app/src/main/java/com/lilith/android/SSHApp/MainActivity.java
@@ -2,8 +2,12 @@ package com.lilith.android.SSHApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,7 +20,10 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MainActivity";
+
     private Button btnExecute;
+    private Button btnConnect;
     private EditText etAddress;
     private EditText etUserName;
     private EditText etPassword;
@@ -25,66 +32,138 @@ public class MainActivity extends AppCompatActivity {
 
     JSch jsch = new JSch();
     Session session;
-    ChannelExec channel;
+    boolean isConnection = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnConnect = findViewById(R.id.btnConnect);
         btnExecute = findViewById(R.id.btnExecute);
         etAddress = findViewById(R.id.etAddress);
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
         etCommand = findViewById(R.id.etCommand);
 
-        btnExecute.setOnClickListener(v -> execute());
+        btnConnect.setOnClickListener(v -> {
+            if (isConnection)
+                disconnect();
+            else{
+                connect();
+
+                // 記憶連線資訊
+                SharedPreferences sp = getSharedPreferences("ssh_connection", MODE_PRIVATE);
+                sp.edit()
+                        .putString("address", etAddress.getText().toString())
+                        .putString("username", etUserName.getText().toString())
+                        .apply();
+            }
+        });
+        btnExecute.setOnClickListener(v -> {
+            if (isConnection)
+                execute();
+            else
+                Toast.makeText(this, "尚未連線", Toast.LENGTH_SHORT).show();
+        });
+
+        // 取得連線記憶資訊
+        SharedPreferences sp = getSharedPreferences("ssh_connection", MODE_PRIVATE);
+        etAddress.setText(sp.getString("address", ""));
+        etUserName.setText(sp.getString("username", ""));
     }
 
-    private void execute(){
-        AsyncTask<Integer, Void,String> executeSSHCommandAsync = new AsyncTask<Integer, Void,String>(){
+    private void execute() {
+        AsyncTask<Void, Void, String> executeCommandAsync = new AsyncTask<Void, Void, String>() {
             @Override
-            protected String doInBackground(Integer... integers) {
-                String message = executeSSHCommand();
+            protected String doInBackground(Void... integers) {
+                String message = executeCommand();
                 return message;
             }
 
             @Override
             protected void onPostExecute(String message) {
-                runOnUiThread(()->{
+                runOnUiThread(() -> {
                     Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                 });
             }
         };
 
-        executeSSHCommandAsync.execute(1);
+        executeCommandAsync.execute();
     }
 
-    private String executeSSHCommand() {
+    private void connect() {
+        AsyncTask<Void, Void, String> connectAsync = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... integers) {
+                String message = executeSSHConnect();
+                if (session.isConnected()) {
+                    isConnection = true;
+                    btnConnect.setText("中斷連線");
+                }
+                return message;
+            }
+
+            @Override
+            protected void onPostExecute(String message) {
+                runOnUiThread(() -> {
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                });
+            }
+        };
+
+        connectAsync.execute();
+    }
+
+    private void disconnect() {
+        if (session.isConnected())
+            session.disconnect();
+
+        isConnection = false;
+        btnConnect.setText("開始連線");
+    }
+
+    private String executeSSHConnect() {
         String message = null;
         try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            Session session = jsch.getSession(etUserName.getText().toString(), etAddress.getText().toString(), 22);
+            session = jsch.getSession(etUserName.getText().toString(), etAddress.getText().toString(), 22);
             session.setPassword(etPassword.getText().toString());
             session.setConfig("StrictHostKeyChecking", "no");
             session.setTimeout(10000);
             session.connect();
-            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+
+            message = "連線成功";
+        } catch (Exception e) {
+            message = "連線失敗" + e.getLocalizedMessage();
+            Log.d(TAG, message);
+        }
+        return message;
+    }
+
+    private String executeCommand() {
+        String message = null;
+        ChannelExec channel = null;
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            channel = (ChannelExec) session.openChannel("exec");
             channel.setCommand(etCommand.getText().toString());
             channel.setOutputStream(outputStream);
             channel.connect();
 
+            while (channel.getExitStatus() == -1) {
+                try {
+                    Thread.sleep(300);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+
             message = new String(outputStream.toByteArray());
         } catch (Exception e) {
-            runOnUiThread(()->{
-                Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-            });
+            message = e.getLocalizedMessage();
         } finally {
-            if(channel != null && channel.isConnected()){
+            if (channel != null && channel.isConnected()) {
                 channel.disconnect();
-            }
-            if(session != null && session.isConnected()){
-                session.disconnect();
             }
         }
 
